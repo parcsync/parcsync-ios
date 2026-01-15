41dca79380f49f9c5266001e03ba48c2eaa45b38
Deni <wisdomochiche@gmail.com>
2026-01-15 07:50:42 +0100

    Initial Commit

---
+++ ParcSync/ParcSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParcSyncApp.swift
+//  ParcSync
+//
+//  Created by Deni Ochiche on 15/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ParcSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
